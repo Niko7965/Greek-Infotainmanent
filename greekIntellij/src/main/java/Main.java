b7f1d86e232a2481e59aa6e1cbefc17deefed935
Niko7965 <54095558+Niko7965@users.noreply.github.com>
2020-03-17 10:25:26 +0100

    Setup2

--- greekIntellij/src/main/java/Main.java
+++ greekIntellij/src/main/java/Main.java
@@ -1,4 +1,25 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+
+
+    public static void main(String[] args){
+        PApplet.main("Main");
+
+    }
+
+    public void settings(){
+        size(1200,800);
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void  draw(){
+
+
+    }
+
 }
