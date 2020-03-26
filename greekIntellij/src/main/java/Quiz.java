4aaa0f29afa70409cf8238097bcbccc5853cdf70
chri81e6 <54137140+chri81e6@users.noreply.github.com>
2020-03-26 16:44:16 +0100

    #11

--- greekIntellij/src/main/java/Quiz.java
+++ greekIntellij/src/main/java/Quiz.java
@@ -4,27 +4,24 @@ import processing.core.PImage;
 import processing.data.Table;
 import processing.event.KeyEvent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import static processing.core.PConstants.CORNERS;
 import static processing.core.PConstants.UP;
 
 public class Quiz {
     PApplet core;
-
     int quizNumber=1;
-
     String question;
-
     String answer;
-
     Table questions;
 
 
     public Quiz(PApplet core, Main main){
-
-
-
         this.quizNumber=quizNumber;
-
         this.core= core;
 
     }
@@ -35,22 +32,46 @@ public class Quiz {
         core.fill(0,0,255,120);
         core.strokeWeight(4);
         core.stroke(240,240,255,200);
-        core.rect(1,core.height-3,core.width-2,core.height-core.height/4,7);
+
+        float qboxy = core.height-3;
+        float qboxheight =core.height-core.height/4;
+        float qboxwidth = core.width-2;
+        core.rect(1,qboxy,qboxwidth,qboxheight,7);
         core.text("despacito",core.width/2,core.height/2);
 
-        quizOption(1,answer);
+        core.textSize(core.width/30);
+        core.textAlign(core.LEFT,core.TOP);
+        core.fill(255,255,255,255);
+        core.text(QuestionSplitter(QuestionString(0),1),qboxwidth/20,qboxy-(qboxheight/3)+5);
+
+        //quizOption(1,answer);
 
     }
 
     public void quizOption(int placement, String text) {
-
-
     }
 
-    public String Question(){
-        
+    public String QuestionString(int n){
+        ArrayList<String> questionlist = new ArrayList<String>();
+        Scanner scan = null;
+        try {
+            scan = new Scanner(new File("files/Questions.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scan.hasNextLine()) {
+            questionlist.add(scan.nextLine());
+        }
+        scan.close();
 
+        //System.out.println(characterlist.get(5));
+        return questionlist.get(n);
+    }
 
+    String QuestionSplitter(String sentence, int n){
+        String[] answers = sentence.split("-");
+        //System.out.println(answers[1]);
+        return answers[n];
     }
 
 }
